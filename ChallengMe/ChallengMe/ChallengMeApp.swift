e014c87ad61c42ee13d0f2d1e743cd63172c5c59
Victorr501 <vrubinr501@gmail.com>
2026-04-09 23:31:37 +0200

    Despliegue de la aplicacion

---
+++ ChallengMe/ChallengMe/ChallengMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengMeApp.swift
+//  ChallengMe
+//
+//  Created by Victor Rubin on 09/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct ChallengMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
